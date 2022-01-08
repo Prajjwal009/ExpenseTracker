be74eb8ccdd011bd8c9bcfa8f8d7bd39db6b31b0
Prajjwal009 <sinhaprajjwal009@gmail.com>
2022-01-08 12:07:13 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Prajjwal on 08/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
